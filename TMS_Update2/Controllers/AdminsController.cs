7d640d852c1c3bda6d89a3e34689c2bdb53aa1b1
Thong Dau Xuan <xuanthongdau@gmail.com>
2020-10-19 17:02:42 +0700

    Update: Change pass

--- TMS_Update2/Controllers/AdminsController.cs
+++ TMS_Update2/Controllers/AdminsController.cs
@@ -16,12 +16,12 @@ namespace TMS_Update2.Controllers
         private ApplicationDbContext _context;
 
         public AdminsController()
-		{
+        {
             _context = new ApplicationDbContext();
-		}
+        }
 
         public ActionResult Index()
-		{
+        {
             var userInfor = (from user in _context.Users
                              select new
                              /*FROM-IN: xác định nguồn dữ liệu truy vấn (Users). 
@@ -31,25 +31,24 @@ namespace TMS_Update2.Controllers
                                  UserId = user.Id,
                                  Username = user.UserName,
                                  Emailaddress = user.Email,
-								 RoleName = (from userRole in user.Roles
-									        //JOIN kết hợp 2 trường dữ liệu tương ứng
-											join role in _context.Roles //JOIN-IN: chỉ ra nguồn kết nối vs nguồn của FROM   
-											on userRole.RoleId          //ON: chỉ ra sự ràng buộc giữa các phần tử  
-											equals role.Id              //EQUALS: chỉ ra căn cứ vs ràng buộc (userRole.RoleId ~~ role.Id)
-                                            select role.Name).ToList()
-							 }
+                                 RoleName = (from userRole in user.Roles
+                                             //JOIN kết hợp 2 trường dữ liệu tương ứng
+                                             join role in _context.Roles //JOIN-IN: chỉ ra nguồn kết nối vs nguồn của FROM   
+                                             on userRole.RoleId          //ON: chỉ ra sự ràng buộc giữa các phần tử  
+                                             equals role.Id              //EQUALS: chỉ ra căn cứ vs ràng buộc (userRole.RoleId ~~ role.Id)
+                                             select role.Name).ToList()
+                             }
                              ).ToList().Select(p => new UserInRolesViewModel()
-                                                {
-                                                    UserId = p.UserId,
-                                                    Username = p.Username,
-                                                    Email = p.Emailaddress,
-                                                    Role = string.Join(",", p.RoleName)  //Lúc review nhớ hỏi thầy vì răn dùng cái ni 
-
-                                                }
-                                                );
+                                             {
+                                                 UserId = p.UserId,
+                                                 Username = p.Username,
+                                                 Email = p.Emailaddress,
+                                                 Role = string.Join(",", p.RoleName)  
+                                             }
+                                             );
 
             return View(userInfor);
-		}
+        }
 
         /*.
 		*//*SHOW ACCOUNT*//*
@@ -62,26 +61,26 @@ namespace TMS_Update2.Controllers
 		}
         .*/
 
-		//DELETE ACCOUNT
-		[HttpGet]
+        //DELETE ACCOUNT
+        [HttpGet]
         public ActionResult Delete(string id)
-		{
+        {
             var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
 
             if (AccountInDB == null)
-			{
+            {
                 return HttpNotFound();
-			}
+            }
 
             _context.Users.Remove(AccountInDB);
-            _context.SaveChanges();       
+            _context.SaveChanges();
             return RedirectToAction("Index");
-		}
+        }
 
         //Edit 
         [HttpGet]
         public ActionResult Edit(string id)
-		{
+        {
             var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
             if (AccountInDB == null)
             {
@@ -91,7 +90,7 @@ namespace TMS_Update2.Controllers
         }
 
         //EDIT
-        [Authorize (Roles = "Admin")]
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public ActionResult Edit(ApplicationUser user)
         {
@@ -103,7 +102,7 @@ namespace TMS_Update2.Controllers
                                   Any(p => p.UserName.Contains(user.UserName));
 
             if (UsernameIsExist)
-			{
+            {
                 ModelState.AddModelError("UserName", "Username already existed");
                 return View();
             }
@@ -135,47 +134,32 @@ namespace TMS_Update2.Controllers
 
 
         //Change password
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public ActionResult ChangePass(string id)
         {
             var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == id);
-            if (AccountInDB == null)
-            {
-                return HttpNotFound();
-            }
-            return View(AccountInDB);
-        }
-
-        //Change password
-        [Authorize(Roles = "Admin")]
-        [HttpPost]
-        public ActionResult ChangePass (ApplicationUser user)
-		{
-            if (!ModelState.IsValid)
-            {
-                return View();
-            }
-
-
-            var AccountInDB = _context.Users.SingleOrDefault(p => p.Id == user.Id);
 
             if (AccountInDB == null)
             {
                 return HttpNotFound();
             }
 
+            //Khai báo biến var userId thuộc Curent.User.Identity và truy cập vào trường Id thông qua GetUserId       
             var userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
-			userId = user.Id;
-            if (userId != null)
+            userId = AccountInDB.Id;
+            if (userId != null) //Nếu userId tồn tại
             {
+                //userManager bằng quản lý người dùng mới, mang dữ liệu mới 
                 UserManager<IdentityUser> userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>());
+                //Xoá password hiện tại của userManager 
                 userManager.RemovePassword(userId);
-                String newPassword = user.PhoneNumber;
+                //Thay password mới "123456789" cho userManager
+                String newPassword = "123456789";
                 userManager.AddPassword(userId, newPassword);
             }
             _context.SaveChanges();
             return RedirectToAction("Index");
         }
-
     }
 }
